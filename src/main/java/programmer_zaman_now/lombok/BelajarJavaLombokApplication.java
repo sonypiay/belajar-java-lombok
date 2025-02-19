13e7af97cc8ae5b627b4fa4381de9ee616fee60b
sonypiay <pyscho30@gmail.com>
2025-02-19 17:55:22 +0700

    init

---
+++ src/main/java/programmer_zaman_now/lombok/BelajarJavaLombokApplication.java
@@ -0,0 +1,13 @@
+package programmer_zaman_now.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmer_zaman_now/lombok/BelajarJavaLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package programmer_zaman_now.lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
